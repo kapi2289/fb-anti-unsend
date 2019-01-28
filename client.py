6a7e19925c993fe67d57488c414a225c1e08dde0
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-01-28 20:36:48 +0100

    Add caching messages

--- client.py
+++ client.py
@@ -7,7 +7,10 @@ class Client(fbchat.Client):
 
     def __init__(self, *args, **kwargs):
         super(Client, self).__init__(*args, **ķwargs)
-        self.messages = dict()
+        self.messages = list()
+
+    def onMessage(self, message_object, thread_id, **kwargs):
+        self.messages.append(message_object)
 
 
 if __name__ == "__main__":
