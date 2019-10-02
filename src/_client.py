a6e15dbcbf0bac000f99a2e8b80e3ea999776ea8
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-10-02 13:40:07 +0200

    Add sending attachments instead of urls

--- src/_client.py
+++ src/_client.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import re
 import time
 
 import fbchat
+import requests
 from fbchat.models import *
 
 from ._settings import *
@@ -15,7 +17,8 @@ class Client(fbchat.Client):
         self.messages = list()
         self.setDefaultThread(self.uid, ThreadType.USER)
 
-    def onMessage(self, message_object, thread_id, **kwargs):
+    def onMessage(self, mid, message_object, thread_id, **kwargs):
+        message_object.uid = mid
         self.messages.append(message_object)
         for message in self.messages:
             ts = (time.time() - 10 * 60) * 1000
@@ -28,25 +31,35 @@ class Client(fbchat.Client):
         for message in self.messages:
             if message.uid == mid:
                 files = []
+                unsendable_files = []
                 for a in message.attachments:
                     if isinstance(a, ImageAttachment):
-                        url = a.large_preview_url
+                        if a.is_animated:
+                            files.append(a.animated_preview_url)
+                        else:
+                            url = a.large_preview_url or a.preview_url or a.thumbnail_url
+                            if url:
+                                files.append(url)
                     elif isinstance(a, VideoAttachment):
-                        url = a.preview_url
+                        files.append(a.preview_url)
                     elif isinstance(a, AudioAttachment):
-                        url = a.url
+                        unsendable_files.append(a.url)
                     elif isinstance(a, FileAttachment):
-                        url = a.url
-                    else:
-                        continue
-                    files.append(url)
+                        r = requests.get(a.url)
+                        if r.status_code == 200:
+                            url = re.search("document\.location\.replace\(\"(.*)\"\);", r.text).group(1)
+                            url = url.replace('\\/', '/')
+                            files.append(url)
+
                 author = self.fetchUserInfo(message.author)[message.author]
                 message.reply_to_id = None
                 self.send(Message("{} unsent the message:".format(author.name),
                                   mentions=[Mention(author.uid, length=len(author.name))]))
-                if files:
-                    self.sendMessage("Attachments: \n{}".format("\n----------\n".join(files)))
                 if message.text or message.sticker:
                     self.send(message)
+                if unsendable_files:
+                    self.sendMessage("Attachments: \n{}".format("\n----------\n".join(unsendable_files)))
+                if files:
+                    self.sendRemoteFiles(files)
                 self.messages = list(filter(lambda x: x is not message, self.messages))
                 break
